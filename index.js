87a12300d21aacb10596c19608b0947479f1cf7c
supreen <62968684+supreen@users.noreply.github.com>
2020-06-12 20:53:02 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,372 @@
+
+
+
+//basic constants and functions
+var checked;
+var checking = [];
+const Discord = require("discord.js");
+const {token,usernamesp, roles, apiKey, spreadsheetId, range, refreshrate} = require("./config/discord.json");
+console.log(token+" "+usernamesp+" "+roles+" "+apiKey+" "+spreadsheetId+" "+range+" "+refreshrate)
+const { google } = require("googleapis");
+
+  const connection = google.sheets({
+  version: "v4",
+  auth: apiKey
+});
+
+
+const client = new Discord.Client();
+
+let oldRows = [];
+
+
+//checking for errors
+const logError = error => {
+  console.error("error log " + new Date());
+  console.error(error);
+};
+
+//the spreadsheet fetching
+const fetchRows = async (spreadsheetId, range, sheetsConnection) => {
+  try {
+    const response = await sheetsConnection.spreadsheets.values.get({
+      spreadsheetId: spreadsheetId,
+      range: range
+    });
+    //fetching all data
+    const rows = response.data.values;
+
+
+    
+    
+    
+    
+    
+    
+    //dividing the data between different array using while loop
+    var i = 0;
+    var k=0;
+   
+    
+    
+    while (i < rows.length) {
+ 
+   
+      const checkrow="";
+      
+   
+      
+      
+      const rows123= rows[i]
+ 
+      
+      
+   
+        
+//finally putting all roles together
+
+      
+      
+const therolenames = rows123[1]+","+rows123[2]+","+rows123[3]+","+rows123[4];
+console.log(rows123[0].trim()+"   "+therolenames.trim());
+    
+      assignRoles(rows123[0].trim(), therolenames.trim());
+  
+
+
+         rows123[0]==null;
+   rows123[1]==null;
+   rows123[2]==null;
+   rows123[3]==null;
+   rows123[4]==null;   rows123[1]==null;
+   roles[1]==null;
+   roles[2]==null;
+   roles[3]==null;
+   roles[4]==null;
+   
+      
+    therolenames==null;
+   roles.length=0;   
+rows123.length=0;
+i++;
+k++;
+       
+      }
+   
+    return rows;
+  } catch (error) {
+    logError(error);
+  }
+};
+
+const assignRoles = async (usernames, roleNames) => {
+
+   var rolenames1 = roleNames.split(",");
+
+  try {
+    const guildMembers = client.guilds.array()[0].members.array();
+ 
+    //number of all members in the group
+ 
+    
+    const guildRoles = client.guilds.array()[0].roles.array();
+    
+  
+    // * getting Role instances of role names
+    const Roles = guildRoles.filter(Role => roleNames.includes(Role.name));
+   //role number of all members in the group
+   
+
+    roleNames.length=0;
+    // * getting GuildMember instances of usernames and setting roles
+    guildMembers.forEach(async member => {
+     
+      
+      const username = member.user.username + "#" + member.user.discriminator;
+   
+      
+          
+          
+      
+  
+  
+      if (usernames.includes(username)) {
+        
+        const notAssignedRoles = [];
+        
+       
+      
+        
+        
+        
+        
+        Roles.forEach(role => {
+       
+         //if (!member.roles.array().includes(Roles)) {
+         //if (!guildRoles.includes(Roles)) {
+          
+          
+      
+      checked="no"
+          
+           
+                  //Check whether the roles (discord) are in roles (spreadsheet)
+          
+          
+          
+             if (Roles.includes(member.roles.array()[1]) || member.roles.array()[1]==null){
+             }else{checked="yes" }    
+               if (Roles.includes(member.roles.array()[2]) || member.roles.array()[2]==null){
+               }else{checked="yes"; }    
+                 if (Roles.includes(member.roles.array()[3]) || member.roles.array()[3]==null){
+                 }else{checked="yes"; }    
+                   if (Roles.includes(member.roles.array()[4]) || member.roles.array()[4]==null){
+                   }else{checked="yes"; }    
+          
+                    
+                           //Check whether the roles (Spreadsheet) are in roles (Discord)
+           
+                    
+              if (member.roles.array().includes(Roles[0]) || Roles[0]==null){
+              }else{checked="yes"; }    
+                  if (member.roles.array().includes(Roles[1]) || Roles[1]==null){
+                  }else{checked="yes"; }    
+                      if (member.roles.array().includes(Roles[2]) || Roles[2]==null){
+                      }else{checked="yes"; }    
+                          if (member.roles.array().includes(Roles[3]) || Roles[3]==null){
+                         }else{checked="yes"; }    
+                            
+                    
+                      if (checked=="yes"){ 
+                        notAssignedRoles.push(role);
+                      }
+
+        });
+        
+        if (notAssignedRoles.length > 0) {
+        
+        await member.removeRoles(guildRoles); member.addRoles(Roles);
+        
+      
+          console.log(
+            "Assigned " +
+              notAssignedRoles.map(role => role.name) +
+              " to " +
+              username + " on " + new Date().toString()
+          );
+        } else {
+              
+          console.log(username + " already has all the roles assigned");
+        }
+      }
+    });
+  } catch (err) {
+    logError(err);
+  }
+};
+
+
+
+const assignRoles2 = async (usernames, roleNames,) => {
+
+ 
+  try {
+    const guildMembers = client.guilds.array()[0].members.array();
+ 
+    //number of all members in the group
+ 
+    
+    const guildRoles = client.guilds.array()[0].roles.array();
+    
+  
+    // * getting Role instances of role names
+    const Roles = guildRoles.filter(Role => roleNames.includes(Role.name));
+   //role number of all members in the group
+   
+
+    roleNames.length=0;
+    // * getting GuildMember instances of usernames and setting roles
+    guildMembers.forEach(async member => {
+     
+      
+      const username = member.user.username + "#" + member.user.discriminator;
+   
+      
+          
+          
+      
+  
+  
+      if (usernames.includes(username)) {
+        
+        const notAssignedRoles = [];
+        
+       
+      
+        
+        
+        
+        
+        Roles.forEach(role => {
+       
+         //if (!member.roles.array().includes(Roles)) {
+         //if (!guildRoles.includes(Roles)) {
+          
+          
+      
+         
+          
+        
+      notAssignedRoles.push(role);      
+                       
+      
+        });
+        if (notAssignedRoles.length > 0) {
+        
+        await member.removeRoles(guildRoles); 
+          await member.addRoles(Roles);
+        
+      var d = new Date().toString();
+          console.log(
+            "Assigned " +
+              notAssignedRoles.map(role => role.name) +
+              " to " +
+              username + " " + d
+          );
+        } else {
+              
+          console.log(username + " already has all the roles assigned");
+        }
+      }
+    });
+  } catch (err) {
+    logError(err);
+  }
+};
+
+
+
+
+
+
+const extractNewEntries = (oldRows, rows) => {
+  let newRows = [];
+  if (rows.length > oldRows.length) {
+    newRows = rows.slice(oldRows.length);
+   
+  }
+  newRows.forEach(row => {
+    oldRows.push(row);
+    
+  });
+  return newRows;
+};
+
+const extractDiscordIDs = rows => {
+  return rows.map(user => user[0]);
+  console.log(rows.map(user => user[0]));
+};
+
+client.once(
+  "ready",
+  () => {
+    console.log("Bot started with these settings:");
+    console.log("• Spreadsheet ID: ");
+    console.log("• Range: " + range);
+    console.log("• Roles: " + roles.join(", "));
+    setInterval(async () => {
+      console.log("\nChecked for new entries on " + new Date().toString());
+      let rows;
+      try {
+        rows = await fetchRows(spreadsheetId, range, connection);
+
+        const newEntries = extractNewEntries(oldRows, rows);
+        if (newEntries.length > 0) {
+          console.log(`Found ${newEntries.length} new entries`);
+          const usernames = extractDiscordIDs(newEntries);
+          
+        } else {
+          console.log("No new entries");
+        }
+      } catch (err) {
+        if (rows === undefined) console.error("Google Sheet is empty");
+        logError(err);
+        rows = [];
+      }
+    }, refreshrate); // refresh rate: 60000 milliseconds == 1 minute
+  },
+  logError
+);
+
+client.on("error", logError);
+
+const start = async () => {
+  try {
+    await client.login(token);
+  } catch (err) {
+    logError(err);
+    setTimeout(start, 30000);
+  }
+};
+
+start();
+
+client.on("disconnect", start);
+
+module.exports = {
+  fetchRows,
+  assignRoles,
+  extractNewEntries,
+  extractDiscordIDs
+};
+
+
+
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.use(express.static("public"));
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
